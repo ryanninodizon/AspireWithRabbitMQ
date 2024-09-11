baf8f31e4ef8d01db0ce8c57e7f6aae3bfaad3d9
Ryan Niño Dizon <ryan.dizon@isdfeniqs.com>
2024-09-11 23:49:41 +0200

    Initial Commit

--- AspireWithRabbitMQ.AppHost/Program.cs
+++ AspireWithRabbitMQ.AppHost/Program.cs
@@ -2,10 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitMQ = builder.AddRabbitMQ("messaging").WithManagementPlugin();
 
-builder.AddProject<Projects.AspireWithRabbitmQ_Sender>("sender")
+builder.AddProject<Projects.AspireWithRabbitMQ_Sender>("sender")
 .WithReference(rabbitMQ);
 
-builder.AddProject<Projects.AspireWithRabbitmQ_Receiver>("receiver")
+builder.AddProject<Projects.AspireWithRabbitMQ_Receiver>("receiver")
 .WithReference(rabbitMQ);
 
 builder.Build().Run();
