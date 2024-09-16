902ff8b9e184c38dbfb4cf95ecbe5a7b35576ad1
Ryan Niño Dizon <50763950+ryanninodizon@users.noreply.github.com>
2024-09-16 13:02:38 +0200

    Update Program.cs

--- AspireWithRabbitMQ.AppHost/Program.cs
+++ AspireWithRabbitMQ.AppHost/Program.cs
@@ -3,7 +3,6 @@ var rabbitMQ = builder.AddRabbitMQ("messaging").WithManagementPlugin();
 
 builder.AddProject<Projects.AspireWithRabbitMQ_Sender>("sender")
 .WithReference(rabbitMQ);
-
 builder.AddProject<Projects.AspireWithRabbitMQ_Receiver>("receiver")
 .WithReference(rabbitMQ);
 
